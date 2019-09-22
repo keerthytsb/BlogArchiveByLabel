fe7b5ce2ad1a610f20e00ff3f3715d5ce657f0ee
keerthytsb <55632711+keerthytsb@users.noreply.github.com>
2019-09-22 20:02:37 +0530

    Update BlogArchiveByLabel.js

--- BlogArchiveByLabel.js
+++ BlogArchiveByLabel.js
@@ -41,3 +41,22 @@ function DisplayPosts(PostTitles, PostURLs, PostLabels) {
 document.write('</ul>');
 }
 </script>
+<a style="font-size: 9px; color: #CECECE; float: right; margin-top: 5px;" href="http://helplogger.blogspot.com/2014/11/5-cool-recent-post-widgets-for-blogger.html" rel="nofollow">Recent Posts Widget</a>
+<noscript>Your browser does not support JavaScript!</noscript>
+<link href='http://fonts.googleapis.com/css?family=Lobster|Gloria+Hallelujah' rel='stylesheet' type='text/css' />
+<style type="text/css">
+img.recent-post-thumb {width:50px;height:50px;float:right;margin: -4px -35px 0px 0px; border: 4px solid #FCD6CB; border-radius: 100%;}
+.recent-posts-container {font-family: 'Gloria Hallelujah', cursive;  float: left;width: 100%;min-height: 55px;margin: 5px 0px 5px 0px;padding: 0;font-size:12px;}
+ul.recent-posts-container {counter-reset: countposts;list-style-type: none; background: #fff; }
+ul.recent-posts-container li:before {content: counter(countposts,decimal);counter-increment: countposts;z-index: 2;position:absolute; left: -20px; font-size: 16px;color: #616662;background: #FCD6CB;padding: 9px 14px; border-radius: 100%; margin-top: 15px;}
+ul.recent-posts-container li {padding:5px 0px;min-height:50px; list-style-type: none; margin: -2px 5px 5px 5px;  border-top: 2px solid #FCD6CB;}
+ul.recent-posts-container {border: 2px solid #FCD6CB; }
+.recent-posts-container a { text-decoration:none; }
+.recent-posts-container a:hover { color: #222;}
+.post-date {color:#e0c0c6; font-size: 11px; }
+.recent-post-title a {font-size: 14px;color: #616662;}
+.recent-post-title {padding: 6px 0px;}
+.recent-posts-details a{ color: #888;}
+.recent-posts-details {padding-bottom: 5px;}
+a.readmorelink {color: #4DACE3;}
+</style>
