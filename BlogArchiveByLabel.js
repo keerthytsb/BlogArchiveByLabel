e7aaf656126a2026824d3603a640e9b2e54a5bdb
keerthytsb <55632711+keerthytsb@users.noreply.github.com>
2019-09-22 20:06:46 +0530

    Update BlogArchiveByLabel.js

--- BlogArchiveByLabel.js
+++ BlogArchiveByLabel.js
@@ -1,46 +1,14 @@
 <script type="text/javascript">
-
-function LoadDetails(TotalFeed) {
- var PostTitles = new Array();
- var PostURLs = new Array();
- var PostLabels=new Array();
- var d="";
- if ("entry" in TotalFeed.feed) {
- var PostEntries = TotalFeed.feed.entry.length;
- for (var PostNum = 0; PostNum < PostEntries; PostNum++) {
-	 var ThisPost = TotalFeed.feed.entry[PostNum];
-	 PostTitles.push(ThisPost.title.$t);
-	 d="";
-	if("category"in TotalFeed.feed.entry[PostNum])
-	{
-	for(var b=0;b<TotalFeed.feed.entry[PostNum].category.length;b++)
-		d+='<a href="/search/label/'+TotalFeed.feed.entry[PostNum].category[b].term+'?max-results=8">'+TotalFeed.feed.entry[PostNum].category[b].term+"</a> \u002C "; 
-		b=d.lastIndexOf(" \u002C ");-1!=b&&(d=d.substring(0,b))
-	}
-	 
-	 var ThisPostURL;
-	 for (var LinkNum = 0; LinkNum < ThisPost.link.length; LinkNum++) {
-		 if (ThisPost.link[LinkNum].rel == "alternate") {
-		 ThisPostURL = ThisPost.link[LinkNum].href;
-		 break;
-		 }
-	 }
-	 PostURLs.push(ThisPostURL);
-	 PostLabels.push(d);
- }
- }
- DisplayPosts(PostTitles, PostURLs, PostLabels);
-}
-
-function DisplayPosts(PostTitles, PostURLs, PostLabels) {
- var NumberOfEntries = PostTitles.length;
- document.write('<ul class="recent-posts-container">');
- for (var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++) {
-	 document.write('<li><a href ="' + PostURLs[EntryNum] + '">' + PostTitles[EntryNum] + "</a> | <span>"+PostLabels[EntryNum]+"</span></li>");
- }
-document.write('</ul>');
-}
+function showlatestpostswiththumbs(t){document.write('<ul class="recent-posts-container">');for(var e=0;e<posts_no;e++){var r,n=t.feed.entry[e],i=n.title.$t;if(e==t.feed.entry.length)break;for(var o=0;o<n.link.length;o++){if("replies"==n.link[o].rel&&"text/html"==n.link[o].type)var l=n.link[o].title,m=n.link[o].href;if("alternate"==n.link[o].rel){r=n.link[o].href;break}}var u;try{u=n.media$thumbnail.url}catch(p){s=n.content.$t,a=s.indexOf("<img"),b=s.indexOf('src="',a),c=s.indexOf('"',b+5),d=s.substr(b+5,c-b-5),u=-1!=a&&-1!=b&&-1!=c&&""!=d?d:"http://2.bp.blogspot.com/-C3Mo0iKKiSw/VGdK808U7rI/AAAAAAAAAmI/W7Ae_dsEVAE/s1600/no-thumb.png"}var h=n.published.$t,f=h.substring(0,4),w=h.substring(5,7),v=h.substring(8,10),A=new Array;A[1]="Jan",A[2]="Feb",A[3]="Mar",A[4]="Apr",A[5]="May",A[6]="Jun",A[7]="Jul",A[8]="Aug",A[9]="Sep",A[10]="Oct",A[11]="Nov",A[12]="Dec",document.write('<li class="recent-posts-list">'),1==posts_date&&document.write('<div class="post-date">'+A[parseInt(w,10)]+" "+v+" "+f+"</div>"),1==showpoststhumbs&&document.write('<a href="'+r+'"><img class="recent-post-thumb" src="'+u+'"/></a>'),document.write('<div class="recent-post-title"><a href="'+r+'" target ="_top">'+i+"</a></div>");var g="",k=0;document.write('<div class="recent-posts-details">'),1==showcommentslink&&(1==k&&(g+=" <br> "),"1 Comments"==l&&(l="1 Comment"),"0 Comments"==l&&(l="No Comments"),l='<a href="'+m+'" target ="_top">'+l+"</a>",g+=l,k=1),1==readmorelink&&(1==k&&(g+=" | "),g=g+'<a class="readmorelink" href="'+r+'" class="url" target ="_top">Read more</a>',k=1),document.write(g),document.write("</div>"),document.write("</li>")}document.write("</ul>")}
 </script>
+<script type="text/javascript">
+var posts_no = 5;
+var showpoststhumbs = true;
+var readmorelink = true;
+var showcommentslink = true;
+var posts_date = true;
+</script>
+<script src="/feeds/posts/default?orderby=published&alt=json-in-script&callback=showlatestpostswiththumbs" rel="nofollow"></script>
 <a style="font-size: 9px; color: #CECECE; float: right; margin-top: 5px;" href="http://helplogger.blogspot.com/2014/11/5-cool-recent-post-widgets-for-blogger.html" rel="nofollow">Recent Posts Widget</a>
 <noscript>Your browser does not support JavaScript!</noscript>
 <link href='http://fonts.googleapis.com/css?family=Lobster|Gloria+Hallelujah' rel='stylesheet' type='text/css' />
